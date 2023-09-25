57d870ad1c0358e98a9b069f54cf728469aaf3e6
renan823 <renan.trofino@gmail.com>
2023-09-25 10:42:01 -0300

    first commit

---
+++ src/pages/_app.jsx
@@ -0,0 +1,11 @@
+import '../styles/globals.css';
+
+function MyApp () {
+    return (
+        <div>
+            <h1 className="text-center text-2xl m-4 text-teal-600 font-bold">Hello World</h1>
+        </div>
+    )
+}
+
+export default MyApp;
